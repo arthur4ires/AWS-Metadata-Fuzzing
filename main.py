c39ac74390ef8c09ebfc0e078615d0c9d9b2715f
Arthur A <aires.arthur.15@gmail.com>
2021-04-08 05:20:00 -0300

    aws access

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-
 import requests
-
+from base64 import b64decode
+from json import loads
+import sys
 
 if "__main__" == __name__:
-	print('ola')
+
+	if len(sys.argv) > 1:
+
+		urlScrapper = ('{}http://169.254.169.254/latest/').format(sys.argv[1])
+
+		firstResponse =  requests.get(urlScrapper).text
+
+		if '{"page":' in firstResponse:
+
+			htmlMetadata =  b64decode(loads(firstResponse)['page']).decode('utf-8')
+
+		else:
+
+			htmlMetadata = firstResponse
+
+		for a in htmlMetadata.split('\n'):
+			print(a)
